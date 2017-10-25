17bc0e4fe14b2a06178342467b4a86c11c8ecdc1
David Mertl <dmertl@gmail.com>
2017-10-24 17:08:34 -0700

    Adding dmertl to authors. Bumping version to 0.1.0.

--- songkick/__init__.py
+++ songkick/__init__.py
@@ -2,7 +2,7 @@ from connection import SongkickConnection as Songkick
 
 
 __author__ = 'Matt Dennewitz'
-__version__ = '0.0.1'
+__version__ = '0.1.0'
 __version_info__ = tuple(__version__.split('.'))
 
 
